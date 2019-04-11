4e74d279f7ece34ae7575a785fe34a6cdf399323
Marcelo Contreras <marcelojoc@gmail.com>
2019-04-10 23:49:45 -0300

    primer commit base

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const {
+    from0to,
+    asyncFrom0to
+} = require("./src/counter");
+
+// Llamadas a funciones
+asyncFrom0to(10,3);
+from0to(10, 2);
