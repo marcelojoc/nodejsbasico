4ef41f92d21ab123ac419d82094a7ab1dd35bf78
marceloNotebook <marcelojoc@gmail.com>
2019-04-11 13:23:49 -0300

    emisor de eventos de node

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require ('./src/greet');
+
+greet.emit('shout', 'tuvieja');
+
+
+greet.emit('call', 'marcelojoc', (name)=>{
+
+  console.log('estamos llamando a --'+ name)
+});
