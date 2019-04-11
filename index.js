a035a48003a4f248de35bc1b0388dccb0cfe322c
marceloNotebook <marcelojoc@gmail.com>
2019-04-11 12:46:49 -0300

    manejo de errores

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const errors = require('./src/errors');
+const handle = require('./s');
+
+
+try{
+
+  errors.standardErr.range();
+
+} catch(err){
+
+  console.log('ha ocurrido un error ' + err)
+
+}
