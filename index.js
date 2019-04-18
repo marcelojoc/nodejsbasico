e68bc83d21f06a4b2e57daafcc783e2a49664380
marceloNotebook <marcelojoc@gmail.com>
2019-04-18 14:34:51 -0300

    EXPRESS BASICO

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const  express = require('express');
+
+const app = express();
+
+
+
+app.get("/", (req, res)=>{
+
+  res.send('Estoy en la ruta home')
+
+})
+
+app.get("/app", (req, res)=>{
+
+  res.send('Estoy en la ruta APP')
+
+})
+
+app.get("*", (req, res)=>{
+
+  res.send('Eta ruta no esta definida')
+
+})
+
+app.listen(3000, ()=>{
+  console.log('el servidor esta en funcionamiento en el puerto 3000');
+})
